65b273442c7c6b502c20cb1b4f48eaad9200d82e
paulitamusic <paulitamusic12@hotmail.com>
2016-04-07 22:46:55 -0400

    I tried
    Directions:
    Add code to attendance.py that allows the teacher to enter the letter L (or l) to begin marking students late. When this happens, the current time should be added to both the student's 'attendance' AND 'lates' lists.
    Update:
    The code will mark student late when teacher enters l or L and the time will be recorded in the attendance shelve but I'm not sure if a new shelve would need to be built so that it will mark the student's late days. So I just tried to add it to the shelve that was already built in. I also don't know if I should use if or elif..

--- attendance.py
+++ attendance.py
@@ -11,6 +11,10 @@ while True:
     if entry in ['q', 'Q']:
         s.close()
         break
+    
+    if entry in ['l', 'L']:
+        print (s[entry]['name'] + 'was to late and scanned in at' + t.strftime('%H:%M\n', t.localtime()))
+        s[entry]['attendance'].append(t.time() + t.strftime('%H:%M\n', t.localtime())) s[entry]['lates'].append(t.time())
 
     elif entry not in s.keys():
         print('ID not found; scan again.')
